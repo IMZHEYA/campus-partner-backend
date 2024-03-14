a72b2e36c111f06b28a44b2fae6bad0c5da57f1d
13425 <1342558165@qq.com>
2024-03-14 13:45:27 +0800

    update 给定时任务添加分布式锁

--- src/main/java/com/example/CampusPartnerBackend/job/PreCacheJob.java
+++ src/main/java/com/example/CampusPartnerBackend/job/PreCacheJob.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.CampusPartnerBackend.modal.domain.User;
 import com.example.CampusPartnerBackend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -23,21 +25,39 @@ public class PreCacheJob {
     private RedisTemplate redisTemplate;
     //重点用户的id
     List<Long> mainUserList = Arrays.asList(1L);
+
+    @Resource
+    private RedissonClient redissonClient;
+
     // 秒-分-时-日-月-年
     @Scheduled(cron = "0 13 18 * * *")
     public void doCacheRecommendUser(){
-        for(Long userId : mainUserList){
-            String redisKey = String.format("Campus:partner:%s",userId);
-//        从数据库中查
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            //pageNum当前请求页码   pageSize每页数据条数
-            Page<User> page = userService.page(new Page<>(1, 20), queryWrapper);
-            //写入缓存
-            try {
-                redisTemplate.opsForValue().set(redisKey,page,20, TimeUnit.MINUTES);
-            } catch (Exception e) {
-                log.error("set redisKey error",e);
+        RLock lock = redissonClient.getLock("cp:job:PreCacheJob:doCache:job");
+        try {
+            //能抢到锁，才执行方法
+            if(lock.tryLock(0,-1,TimeUnit.MILLISECONDS)){
+                for(Long userId : mainUserList){
+                    String redisKey = String.format("Campus:partner:%s",userId);
+                    // 从数据库中查
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    //pageNum当前请求页码   pageSize每页数据条数
+                    Page<User> page = userService.page(new Page<>(1, 20), queryWrapper);
+                    //写入缓存
+                    try {
+                        redisTemplate.opsForValue().set(redisKey,page,20, TimeUnit.MINUTES);
+                    } catch (Exception e) {
+                        log.error("set redisKey error",e);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("doCacheRecommendUser error",e);
+        }finally {
+            //只能释放自己的锁
+            if(lock.isHeldByCurrentThread()){
+                lock.unlock();
             }
         }
+
     }
 }
---
+++ src/test/java/com/example/CampusPartnerBackend/service/RedissonTest.java
@@ -0,0 +1,46 @@
+package com.example.CampusPartnerBackend.service;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RList;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+public class RedissonTest {
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    @Test
+    void test(){
+        RList<String> rList = redissonClient.getList("test-redisson");
+        rList.add("zhezi");
+        System.out.println(rList.get(0));
+    }
+
+
+    @Test
+    void testWatchDog() {
+        RLock lock = redissonClient.getLock("yupao:precachejob:docache:lock");
+        try {
+            // 只有一个线程能获取到锁
+            if (lock.tryLock(0, -1, TimeUnit.MILLISECONDS)) {
+                // todo 实际要执行的方法
+                Thread.sleep(10000);
+                System.out.println("getLock: " + Thread.currentThread().getId());
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // 只能释放自己的锁
+            if (lock.isHeldByCurrentThread()) {
+                System.out.println("unLock: " + Thread.currentThread().getId());
+                lock.unlock();
+            }
+        }
+    }
+}
